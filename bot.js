389831b7df1a9de9ac1f6ff629fff7f0fe2fede8
e2k3 <43041483+e2k3@users.noreply.github.com>
2019-06-22 15:56:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const fs = require("fs");
 const prefix = "#" // تقدر تغيره
 	const blow = new Set()
 	client.on('message', message => {
